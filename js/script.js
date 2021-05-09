5e97ce49e2fba1ce15ce2b6f8c4818ec5365f415
VaheAA <VaheAA@github.com>
2021-05-09 18:40:06 +0400

    new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const acc = document.getElementsByClassName("dropdown-hover");
+const arrow = document.getElementsByClassName("arrow");
 let i;
+
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
@@ -11,4 +13,9 @@ for (i = 0; i < acc.length; i++) {
     }    
     panel.classList.toggle("active");
   });
+  if(screen.width <= 600) {
+    document.querySelector('.hover').classList.remove('hover');
+    document.querySelector('.arrow-hover').classList.remove('arrow-hover');
+  }
 }
+
